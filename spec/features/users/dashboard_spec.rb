b80579ea08798f523ead6cd52b5fdba8b8df8590
Arika Turing <ablenker@gmail.com>
2021-03-24 17:36:21 -0600

    Create test for adding friend to friend list

--- spec/features/users/dashboard_spec.rb
+++ spec/features/users/dashboard_spec.rb
@@ -41,15 +41,36 @@ RSpec.describe 'Authenticated User' do
 
   describe "friends section" do
     it 'has search field and button to add friend' do
-      within (".friends-section") do
+      within(".friends-section") do
         expect(page).to have_field(:email, placeholder: 'Search by email')
         expect(page).to have_button("Add Friend")
       end
     end
 
     it 'defaults to no friends' do
-      within (".friends-section") do
+      within(".friends-section") do
         expect(page).to have_content("You currently have no friends.")
+      end
+    end
+
+    it 'when I add an email that is in the database, it gets added to the friends list' do
+      new_friend = User.create!(email: "another@email.com", password: "superssecret")
+
+      within(".friends-list") do
+        expect(page).to_not have_content(new_friend.email)
+      end
+
+      within(".friends-section") do
+        fill_in 'email', with: new_friend.email
+        click_button "Add Friend"
+      end
+
+      within(".friends-list") do
+        expect(page).to_not have_content(new_friend.email)
+      end
+
+      within(".friends-section") do
+        expect(page).to_not have_content("You currently have no friends.")
       end
     end
   end
