826c485a096da6cbcd1f7a646e4e05fa8846673a
Ben Fulton <benjamin.fulton99@gmail.com>
2021-03-24 20:29:45 -0600

    Update user controller with index action and create movies controller with index and show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def index
+
+  end
+
   def show
     @user ||= User.find(session[:user_id]) if session[:user_id]
   end
