cb1584f6a90958b884e2c59238f2e49df4d4889b
Ben Fulton <benjamin.fulton99@gmail.com>
2021-03-24 19:52:44 -0600

    Write basic testing for movie discovery page

---
+++ spec/features/users/discovery_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe 'Authenticated User' do
+  before :each do
+    @user = User.create!(email: "ben@example.com", password: "verysecure")
+    visit root_path
+
+    click_button "Sign In"
+    fill_in :email, with: @user.email
+    fill_in :password, with: 'verysecure'
+
+    click_button "Sign In"
+    click_button "Discover Movies"
+  end
+
+  describe "discovery page" do
+    it "should take me to the discover page" do
+      expect(current_path).to eq(discover_path)
+    end
+    it "should have a button to discover the top 40 movies" do
+      expect(page).to have_button "Find Top Rated Movies"
+    end
+
+    it "should have a text field to search by movie title" do
+
+    end
+
+    it "should have a button to search by movie title" do
+
+    end
+
+    it "should take me to the movies page after clicking either" do
+
+    end
+  end
+end
