668d3c4d09e8c6d8e3f1b5c532b9ad23e5899eaa
Ben Fulton <benjamin.fulton99@gmail.com>
2021-03-24 18:16:09 -0600

    Write login partial and update session controller actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,14 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
-    
+    user = User.find_by(email: params[:email].downcase)
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to dashboard_path
+    else
+      flash[:message] = 'email or password is incorrect'
+      render :new
+    end
   end
 end
