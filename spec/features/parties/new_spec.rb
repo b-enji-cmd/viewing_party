5338f3caa8666b28754491a3c014c8fb7f068bf9
Diana Buffone <diana.pamelaet@gmail.com>
2021-03-27 14:38:45 -0400

    Add webmock and vcr initial setup

--- spec/features/parties/new_spec.rb
+++ spec/features/parties/new_spec.rb
@@ -3,13 +3,20 @@ require 'rails_helper'
 RSpec.describe 'As Authenticated User' do
   describe 'When I visit the new viewing party page' do
     before :each do
-      # get a movie object saved here as test setup, below is temporary?
-      # @movie = Movie.create!(title: "The Fake Film of Movie")
+      @user = User.create!(email: "winner@email.fr", password: "hellomovies2021")
+
+      visit root_path
+
+      click_button "Sign In"
+      fill_in :email, with: @user.email
+      fill_in :password, with: 'hellomovies2021'
+      click_button "Sign In"
+      click_button "Discover Movies"
+      fill_in :q, with: ''
+    end
+    it 'I see the the name of the movie title rendered above a form' do
 
       visit new_party_path
-    end
-
-    it 'I see the the name of the movie title rendered above a form' do
 
       expect(page).to have_content("Your Viewing Party Details")
 
@@ -19,6 +26,7 @@ RSpec.describe 'As Authenticated User' do
     end
 
     it 'and duration of Party with a default value of movie runtime in minutes' do
+      visit new_party_path
 
       within(".new-party-form") do
         expect(page).to have_field("party[duration]")
@@ -28,11 +36,13 @@ RSpec.describe 'As Authenticated User' do
     end
 
     it 'a viewing party should NOT be created if set to a value less than the duration of the movie' do
+      visit new_party_path
 
       fill_in "party[duration]", with: 10
     end
 
     it 'When: field to select date' do
+      visit new_party_path
 
       within(".new-party-form") do
         expect(page).to have_field("party[date]")
@@ -40,24 +50,34 @@ RSpec.describe 'As Authenticated User' do
     end
 
     it 'Start Time: field to select time' do
+      visit new_party_path
+
       within(".new-party-form") do
         expect(page).to have_field("party[start_time]")
       end
     end
 
     it 'Checkboxes next to each friend (if user has friends)' do
+      visit new_party_path
+
     end
 
     it 'and if user has no friends a button to add friends' do
+      visit new_party_path
+
     end
 
     it 'Button to create a party' do
+      visit new_party_path
+
       within(".new-party-form") do
         expect(page).to have_button("Create Party")
       end
     end
 
     it 'and when I click create button, I am redirected back to my dashboard where I see the newly created event' do
+      visit new_party_path
+
 
       click_button("Create Party")
 
@@ -66,6 +86,8 @@ RSpec.describe 'As Authenticated User' do
     end
 
     it 'and  my event should be seen by any friends that were invited once they long in' do
+      visit new_party_path
+
     end
   end
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,6 @@
 require 'simplecov'
 SimpleCov.start
+require 'webmock/rspec'
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -64,9 +65,21 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
      with.test_framework :rspec
      with.library :rails
   end
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+
+  config.filter_sensitive_data('YOU WISH') { ENV["movies_secret"] }
+
+  config.default_cassette_options = { re_record_interval: 30.days }
+
+  config.configure_rspec_metadata!
 end
