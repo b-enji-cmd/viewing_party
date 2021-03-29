8a3db3056b2753c2f09395668fafc79e5e57cc1f
Ben Fulton <benjamin.fulton99@gmail.com>
2021-03-29 16:53:08 -0600

    Refactor API consumption to feature the use of POROs
    Co-authored-by: Arika Blenker <71908665+arikalea@users.noreply.github.com>
    Co-authored-by: Diana Buffone <71909590+Diana20920@users.noreply.github.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,7 +8,8 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = MovieService.find_movie(params[:db_id])
+    binding.pry
+    #@movie = MovieService.find_movie(params[:db_id])
   end
 
 end
--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,9 +1,16 @@
 class MovieService < ApiService
   def self.top_movies
     endpoint = "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV["movies_secret"]}&language=en-US&page=1"
-    create_data(endpoint)
+    #call an additional page if there are less than 40 movies
+    # each with object
+    # get_data inside enum
+    # endpoint = somestringpage=#{blockvariable with some integer}
+    # create MooVee PORO
+    # less than 40 movies?
+    # parse another endpoint page
 
     parsed_top_rated = get_data(endpoint)
+    #This can be 2 pages,
     parsed_top_rated[:results].map do |movie|
       Moovee.new(movie)
     end
@@ -11,7 +18,9 @@ class MovieService < ApiService
 
   def self.search_movie(title)
     search_endpoint = "https://api.themoviedb.org/3/search/movie?api_key=#{ENV["movies_secret"]}&language=en-US&query=#{title}"
-    create_data(search_endpoint)
+    #create_data(search_endpoint)
+    #
+    #keep parsing api until the length of the return is 40 movies
     parsed_search = get_data(search_endpoint)
     parsed_search[:results].map do |movie|
       Moovee.new(movie)
@@ -24,12 +33,14 @@ class MovieService < ApiService
     binding.pry
   end
 
-  def self.create_data(endpoint)
-    parsed_top_rated = get_data(endpoint)
 
-    parsed_top_rated[:results].map do |movie|
-      Movie.create(title: movie[:title]) if Movie.find_by(title: movie[:title]).nil?
-    end.compact
-  end
+  #MOVING THIS METHOD TO THE ACTION WHICH IS THE RESULT OF CREATING A VIEWING PARTY
+  # def self.create_data(endpoint)
+  #   parsed_top_rated = get_data(endpoint)
+  #
+  #   parsed_top_rated[:results].map do |movie|
+  #     Movie.create(title: movie[:title]) if Movie.find_by(title: movie[:title]).nil?
+  #   end.compact
+  # end
 
 end
