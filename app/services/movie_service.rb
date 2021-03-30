d91bc381aede935fee20d51619091cd0cc193168
Ben Fulton <benjamin.fulton99@gmail.com>
2021-03-29 20:47:37 -0600

    Update movie service to return respective results

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -3,13 +3,6 @@ class MovieService < ApiService
     page_one = "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV["movies_secret"]}&language=en-US&page=1"
     page_two = "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV["movies_secret"]}&language=en-US&page=2"
 
-    #call an additional page if there are less than 40 movies
-    # each with object
-    # get_data inside enum
-    # endpoint = somestringpage=#{blockvariable with some integer}
-    # create MooVee PORO
-    # less than 40 movies?
-    # parse another endpoint page
 
     parsed_page_one = get_data(page_one)
     parsed_page_two = get_data(page_two)
@@ -23,16 +16,19 @@ class MovieService < ApiService
   def self.search_movie(title)
     page = 1
     matching_movies = []
+    #
 
     until matching_movies.length == 40
       search_endpoint = "https://api.themoviedb.org/3/search/movie?api_key=#{ENV["movies_secret"]}&language=en-US&query=#{title}&page=#{page}"
       parsed_search = get_data(search_endpoint)
+      break matching_movies if matching_movies.length == parsed_search[:total_results]
+
       parsed_search[:results].each do |movie|
         matching_movies << Moovee.new(movie)
       end
       page += 1
     end
-    matching_movies
+    matching_movies.first(40)
   end
 
   def self.find_movie(id)
